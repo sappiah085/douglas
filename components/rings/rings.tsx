97c9a76aad9d4f0133e59b458f2887e0b30ea774
realmann <sappiah085@st.ug.edu.gh>
2023-03-03 13:27:13 +0100

    new changes

--- components/rings/rings.tsx
+++ components/rings/rings.tsx
@@ -1,6 +1,6 @@
 import { ringsColor } from "../resuse/colors";
 const Circles = ({ color }: { color: string }) => (
-  <div className="absolute aspect-square  z-[2] md:right-[0%] md:translate-x-[50%] top-[71.1%]  md:top-0 right-[50%] md:h-full">
+  <div className="absolute aspect-square  z-[2] md:right-[0%] md:translate-x-[50%] top-[70%]  md:top-0 right-[50%] md:h-full">
     {[`${color}4`, `${color}3`, `${color}2`, `${color}1`].map((color, id) => {
       return (
         <div
@@ -10,7 +10,7 @@ const Circles = ({ color }: { color: string }) => (
             zIndex: 4 - id,
             backgroundColor: ringsColor[color],
           }}
-          className={`absolute rounded-[50%]  top-[50%] aspect-square left-[50%]  translate-x-[-50%] translate-y-[-50%] scale-[0.45] md:scale-100`}
+          className={`absolute rounded-[50%]  top-[50%] aspect-square left-[50%]  translate-x-[-50%] translate-y-[-50%] scale-[0.50] md:scale-100`}
         ></div>
       );
     })}
